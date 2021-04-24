61ef1262ff49e8d48a9330dbef654fe36f48ea9b
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-04-24 14:12:25 +0800

    converted int to float

--- ETL/schema.sql
+++ ETL/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE water (
 id SERIAL PRIMARY KEY, 
 country TEXT,
 year INT,
-accessibility_percentage DECIMAL(3,2)
+accessibility_percentage FLOAT
 );
 
 --Create table for mortalities
@@ -12,5 +12,5 @@ id SERIAL PRIMARY KEY,
 country TEXT,
 year INT,
 gender TEXT,
-mortality_rate DECIMAL(3,2)
+mortality_rate FLOAT
 );
