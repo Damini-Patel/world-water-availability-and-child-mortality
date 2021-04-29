3a36eca48f76211e287d68c63cfce69cc5f723d2
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-04-29 18:56:27 +0800

    added js files

---
+++ Static/water.js
@@ -0,0 +1,88 @@
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 90, left: 40 },
+  width = 460 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+// define the axis
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+// append the svg object to the body of the page
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.json("/api/v1.0/jwater").then(function (error, data) {
+  data.forEach(function (d) {
+    d.country = d.country;
+    d.accessibility_percentage = +d.accessibility_percentage;
+  });
+
+  // X axis
+  var x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(
+      data.map(function (d) {
+        return d.country;
+      })
+    )
+    .padding(0.2);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  // Add Y axis
+  var y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
+  svg.append("g").call(d3.axisLeft(y));
+
+  // Bars
+  svg
+    .selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+      return x(d.country);
+    })
+    .attr("width", x.bandwidth())
+    .attr("fill", "#69b3a2")
+    // no bar at the beginning thus:
+    .attr("height", function (d) {
+      return height - y(0);
+    }) // always equal to 0
+    .attr("y", function (d) {
+      return y(0);
+    });
+
+  // Animation
+  svg
+    .selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("y", function (d) {
+      return y(d.accessibility_percentage);
+    })
+    .attr("height", function (d) {
+      return height - y(d.accessibility_percentage);
+    })
+    .delay(function (d, i) {
+      console.log(i);
+      return i * 100;
+    });
+});
