6ae99dc429558866bcaf3b77266a24eef75afdcf
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-04-29 19:26:45 +0800

    change folder name

--- Static/javascript/combined.js
+++ Static/javascript/combined.js
@@ -5,7 +5,7 @@ var margin = { top: 10, right: 30, bottom: 30, left: 60 },
 
 // append the svg object to the body of the page
 var svg = d3
-  .select("#my_dataviz")
+  .select("#body")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
